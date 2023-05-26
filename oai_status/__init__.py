146347c5d26b5eb84e4a06f0c6e4345300ced1d1
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2023-05-26 15:46:20 +0000

    ci: test on push

--- oai_status/__init__.py
+++ oai_status/__init__.py
@@ -1,9 +1,18 @@
 from delb import Document
 
 
-def list_sets():
-    r = Document('https://oai.sbb.berlin/?verb=ListSets')
+def list_sets(base_url: str = 'https://oai.sbb.berlin/') -> bool:
+    """
+    queries an OAI-PMH service with the ``ListSets`` verb and expects
+    a ``resumptionToken`` in the response. Throws otherwise.
+
+    >>> list_sets('https://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi')
+    True
+
+    """
+    r = Document(f'{base_url}?verb=ListSets')
     assert r.xpath('//resumptionToken')
+    return True
 
     
 def main():
